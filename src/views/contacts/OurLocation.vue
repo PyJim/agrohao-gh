ce3ee3cce40f3c102c2a67880d3db675cffd7adb
Jimmy Essel <jimmy.essel@kibo.school>
2023-10-02 20:05:51 +0000

    contact page

--- src/views/contacts/OurLocation.vue
+++ src/views/contacts/OurLocation.vue
@@ -6,7 +6,7 @@
         
 
         <div class="texts">
-            <h3 id="contact-us"></h3>
+            <h3 id="contact-us">Contact Us</h3>
             <h1 class="text-title">Locate our office</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde inventore corrupti hic consequatur repudiandae in ut deleniti eos, nam vero? Totam tenetur fugit tempore recusandae tempora ducimus impedit dignissimos id, sequi obcaecati maxime, hic, culpa explicabo quidem. Voluptatem natus aperiam iure eveniet sint esse saepe aspernatur libero velit quae quod delectus quisquam repellat et vero quam, provident commodi exercitationem, omnis consequuntur quaerat porro? Expedita voluptatum labore esse similique blanditiis voluptatem aspernatur debitis, ullam, et qui corporis aperiam, maxime iure. Laudantium nostrum expedita ratione quisquam amet, nihil repellendus, iure enim a quod sed perspiciatis, omnis quibusdam quos? Quibusdam mollitia officiis magnam.</p>
             <br>
@@ -44,7 +44,7 @@
         width: 80vw;
         display: flex;
         flex-direction: column;
-        align-items: center;
+        align-items: flex-start;
         justify-content: center;
     }
     .texts h1{
